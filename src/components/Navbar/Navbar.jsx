fca7b639446587f6937795df8c141a7b9da89777
indrareddy15 <gantaindrareddy83@gmail.com>
2024-09-30 22:44:38 +0530

    NavBar setUp

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import HearderLogo from "../../assets/headerLogo.png";
+import MenuIcon from "../../assets/menu.svg";
+import CloseIcon from "../../assets/close.svg";
+import "./Navbar.css";
+
+function Navbar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  return (
+    <header className="header">
+      <img src={HearderLogo} alt="HeaderLogo" />
+      {sidebarOpen ? (
+        <>
+          <nav className="sidebar">
+            <img
+              className="close"
+              src={CloseIcon}
+              alt="CloseIcon"
+              onClick={toggleSidebar}
+            />
+            <a href="/">Find Doctors</a>
+            <a href="/">Hospitals</a>
+            <a href="/">Medicines</a>
+            <a href="/">Surgeries</a>
+            <a href="/">Software for Provider</a>
+            <a href="/">Fecilities</a>
+            <button>My Bookings</button>
+          </nav>
+        </>
+      ) : (
+        <>
+          <nav className="nav">
+            <img src={CloseIcon} alt="MenuIcon" />
+            <a href="/">Find Doctors</a>
+            <a href="/">Hospitals</a>
+            <a href="/">Medicines</a>
+            <a href="/">Surgeries</a>
+            <a href="/">Software for Provider</a>
+            <a href="/">Fecilities</a>
+            <button>My Bookings</button>
+          </nav>
+        </>
+      )}
+      <img src={MenuIcon} alt="MenuIcon" className="menu" />
+    </header>
+  );
+}
+
+export default Navbar;
