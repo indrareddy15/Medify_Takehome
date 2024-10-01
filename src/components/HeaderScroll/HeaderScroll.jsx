1b6b1c34d440255d526a3c6e111a104a84158190
indrareddy15 <gantaindrareddy83@gmail.com>
2024-10-01 20:08:15 +0530

    HeaderScroll fix

---
+++ src/components/HeaderScroll/HeaderScroll.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HeaderScroll.css";
+
+const HeaderScroll = () => {
+  return (
+    <div className="header-scroll">
+      The health and well-being of our patients and their health care team will
+      always be our priority, so we follow the best practices for cleanliness.
+    </div>
+  );
+};
+
+export default HeaderScroll;
